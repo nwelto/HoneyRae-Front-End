4c4bc5deeaeea5803f067e131539c9f301dec2f3
Nathan <nathopp@gmail.com>
2024-02-01 23:09:13 -0600

    fixed circular reference issue

--- src/data/customerData.js
+++ src/data/customerData.js
@@ -1,16 +1,23 @@
+const _apiBase = "/api/customers";
+
 export const getCustomers = () => {
-  return fetch('/api/customers')
-      .then(response => response.json())
+  return fetch(_apiBase)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
       .catch(error => console.error('Error fetching customers:', error));
 };
 
 export const getCustomerById = (customerId) => {
-  return fetch(`/api/customers/${customerId}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .catch(error => console.error('Error fetching customer:', error));
+  return fetch(`${_apiBase}/${customerId}`)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .catch(error => console.error('Error fetching customer:', error));
 };
--- src/data/serviceTicketsData.js
+++ src/data/serviceTicketsData.js
@@ -6,12 +6,11 @@ export const getServiceTickets = () => {
 
 //export a function here that gets a ticket by id
 export const getServiceTicketById = (ticketId) => {
-  return fetch(`/api/servicetickets/${ticketId}`)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .catch(error => console.error('Error fetching ticket:', error));
+  return fetch(`${_apiUrl}/${ticketId}`).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Network response was not ok');
+  })
+  .catch(error => console.error('Error fetching ticket:', error));
 };
